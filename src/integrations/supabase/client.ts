53f36e37b5738ec87491e4535132f0826c68a248
Sandwich <153335053+SandwichOriginal@users.noreply.github.com>
2025-12-12 23:02:50 -0600

    Add files via upload

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,18 +2,16 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const SUPABASE_URL = "https://ezxrjflznhydrmmblxni.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6eHJqZmx6bmh5ZHJtbWJseG5pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1OTc4MjcsImV4cCI6MjA3NTE3MzgyN30.Ks6S9wl9tvG-A5gx1rCnYiV4XTiuk7YYwHLLwaFuw-g";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
 
-export const supabase = SUPABASE_URL && SUPABASE_PUBLISHABLE_KEY 
-  ? createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
-      auth: {
-        storage: localStorage,
-        persistSession: true,
-        autoRefreshToken: true,
-      }
-    })
-  : null;
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
+  auth: {
+    storage: localStorage,
+    persistSession: true,
+    autoRefreshToken: true,
+  }
+});
